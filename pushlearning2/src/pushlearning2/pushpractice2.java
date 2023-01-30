ded0002faacc1c4e3e0ad384f976400991066d31
gpavani21 <123949637+gpavani21@users.noreply.github.com>
2023-01-30 21:33:58 +0530

    Update pushpractice2.java

--- pushlearning2/src/pushlearning2/pushpractice2.java
+++ pushlearning2/src/pushlearning2/pushpractice2.java
@@ -4,6 +4,7 @@ public class pushpractice2 {
 
 	public static void main(String[] args) {
 		System.out.println("iam inside push practice class 2");
+		System.out.println("trying pull concept");
 
 	}
 
